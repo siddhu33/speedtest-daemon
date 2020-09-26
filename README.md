b9b9522fddce75cfba380ad421926296ca061b33
Siddharth Vasu <siddharth_vasu@hotmail.co.uk>
2020-09-26 11:06:15 +0100

    Add client mode, some cleanup, sort by datetime.

--- README.md
+++ README.md
@@ -25,7 +25,24 @@ FLAGS:
 OPTIONS:
     -d, --database <db>           [default: ./speedtest_daemon.db]
     -i, --interval <interval>     [default: 5000]
+    -m, --mode <mode>             [default: server, optional : client]
     -r, --runs <runs>             [default: -1]
 ```
 
-`database` is the existing or default file that this app uses to persist the data, `interval` represents the time between runs of the test, and `runs` simply is the number of times the application will test before closing. The default is -1 and so the application will run indefinitely.
+- `database` is the existing or default file that this app uses to persist the data.
+- `interval` represents the time between runs of the test.
+- `mode` is whether this daemon will run as 'server' or 'client'. Client is simply a debug mode for printing out the current stats.
+- `runs` is the number of times the application will test before closing. The default is -1 and so the application will run indefinitely.
+
+## Current Result Structure
+
+```sql
+SELECT datetime( timestamp, 'unixepoch' ),
+       download,
+       upload,
+       ping,
+       bytes_sent,
+       bytes_received
+FROM results
+ORDER BY datetime;
+```
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use chrono::DateTime;
+use chrono::{DateTime};
 use rusqlite::{params, Connection, Result};
 use serde_json::Value;
 use std::process::Command;
@@ -24,6 +24,17 @@ timestamp, download, upload, ping, bytes_sent, bytes_received
 VALUES (
 ?1, ?2, ?3, ?4, ?5, ?6 
 );";
+
+static READ_QUERY: &str = "
+SELECT datetime( timestamp, 'unixepoch' ),
+       download,
+       upload,
+       ping,
+       bytes_sent,
+       bytes_received
+FROM results
+ORDER BY datetime;
+";
 
 //Create SQL Table if not already exists for SQLite persistance.
 fn create_tables(conn: &Connection) -> Result<()> {
@@ -72,19 +83,16 @@ struct Opt {
 
     #[structopt(short, long, default_value = "-1")]
     runs: i64,
+
+    #[structopt(short="m", long="mode", default_value = "server")]
+    mode: String,
 }
 
 #[macro_use]
 extern crate log;
 
-//Entry point for the daemon, currently just runs once but at some point could schedule to run
-//repeatedly in a loop or until killed.
-fn main() {
-    env_logger::init();
-    let opt = Opt::from_args();
-    let cn = Connection::open(opt.db.as_str()).expect("Could not open connection to file!");
-    create_tables(&cn).expect("Could not create SQL tables!");
-
+fn run_speedtest(opt : &Opt, conn:&Connection) {
+    //Run speedtest in a loop.
     let mut i: i64 = 0;
 
     if opt.runs != -1 {
@@ -111,10 +119,66 @@ fn main() {
             bytes_received: parsed["bytes_received"].as_f64().unwrap(),
         };
 
-        insert_result(&result, &cn).expect("Insert failed!");
+        insert_result(&result, &conn).expect("Insert failed!");
         i += 1;
         let duration = time::Duration::from_millis(opt.interval);
         thread::sleep(duration);
+    }
+}
+
+#[derive(Debug)]
+struct QueryResult {
+    timestamp: String,
+    download: f64,
+    upload: f64,
+    ping: f64,
+    bytes_sent: f64,
+    bytes_received: f64,
+}
+
+fn query_tables( conn : &Connection ) -> Vec<String> {
+    let mut stmt = conn.prepare(READ_QUERY).unwrap();
+    let rows = stmt.query_map(params![], |row| {
+        let mut v : Vec<String> = Vec::new();
+        let r = QueryResult {
+            timestamp : row.get_unwrap(0),
+            download : row.get_unwrap(1),
+            upload : row.get_unwrap(2),
+            ping : row.get_unwrap(3),
+            bytes_sent : row.get_unwrap(4),
+            bytes_received : row.get_unwrap(5),
+        };
+        v.push(r.timestamp);
+        v.push(r.download.to_string());
+        v.push(r.upload.to_string());
+        v.push(r.ping.to_string());
+        v.push(r.bytes_sent.to_string());
+        v.push(r.bytes_received.to_string());
+        Ok( v.join(",") )
+    });
+    let mut out = Vec::new();
+    for row in rows.expect("Query has failed to run!") {        
+        out.push( row.unwrap() );
+    }
+    return out;
+}
+
+//Entry point for the daemon, currently just runs once but at some point could schedule to run
+//repeatedly in a loop or until killed.
+fn main() {
+    env_logger::init();
+    let opt = Opt::from_args();
+    let cn = Connection::open(opt.db.as_str()).expect("Could not open connection to file!");
+    create_tables(&cn).expect("Could not create SQL tables!");
+    if opt.mode == "server" {
+        run_speedtest(&opt, &cn);
+    } else if opt.mode == "client" {
+        let results = query_tables( &cn );
+        for result in results {
+            println!("{}", result);
+        }
+    } else {
+        println!("Mode : {} is not supported in speedtest-daemon!", opt.mode );
     }
     cn.close().expect("Could not close connection to database");
 }
